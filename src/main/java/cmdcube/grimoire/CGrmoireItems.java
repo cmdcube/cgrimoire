af898bd0f5a73e113dd35993e311282ecf08b22f
cmdcubemc-glitch <cmdcubemc@gmail.com>
2026-08-17 22:31:56 +0200

    idk

--- src/main/java/cmdcube/grimoire/CGrmoireItems.java
+++ src/main/java/cmdcube/grimoire/CGrmoireItems.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.function.Function;
 
 public class CGrmoireItems {
-    public static final Item GRIMOIRE = register("grimoire", Item::new, new Item.Properties());
+    public static final Item GRIMOIRE = register("grimoire", Item::new, new Item.Properties().stacksTo(1));
 
     public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
             // Create the item key.
