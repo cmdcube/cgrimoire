ea1a1f22a346e22c991e26552c6db1f730d5fac2
Previoustoaster7 <felixfuhry@gmail.com>
2026-08-17 20:59:30 +0200

    fix message calling itslanker a dumbass

--- src/main/java/cmdcube/grimoire/CGrimoire.java
+++ src/main/java/cmdcube/grimoire/CGrimoire.java
@@ -19,6 +19,7 @@ public class CGrimoire implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("ItsLanker is a goon!");
 		LOGGER.info("[CGrimoire] loaded successfully!");
+        LOGGER.info("1234");
 	}
 
 	public static Identifier id(String path) {
