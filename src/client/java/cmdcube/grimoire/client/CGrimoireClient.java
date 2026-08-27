245aeff279d37b9a79dc17c91e0451a402b4e921
cmdcubemc-glitch <cmdcubemc@gmail.com>
2026-08-27 12:40:02 +0200

    idk

--- src/client/java/cmdcube/grimoire/client/CGrimoireClient.java
+++ src/client/java/cmdcube/grimoire/client/CGrimoireClient.java
@@ -1,10 +1,13 @@
 package cmdcube.grimoire.client;
 
+import cmdcube.grimoire.CGrimoire;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.minecraft.client.particle.EndRodParticle;
 
 public class CGrimoireClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ParticleFactoryRegistry.getInstance().register(CGrimoire.SPARKLE_PARTICLE, EndRodParticle.Provider::new);
 	}
 }
--- src/main/java/cmdcube/grimoire/CGrimoire.java
+++ src/main/java/cmdcube/grimoire/CGrimoire.java
@@ -2,6 +2,10 @@ package cmdcube.grimoire;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.core.Registry;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 
 import org.slf4j.Logger;
@@ -10,6 +14,7 @@ import org.slf4j.LoggerFactory;
 public class CGrimoire implements ModInitializer {
 	public static final String MOD_ID = "cgrimoire";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final SimpleParticleType SPARKLE_PARTICLE = FabricParticleTypes.simple();
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +22,7 @@ public class CGrimoire implements ModInitializer {
 		CGrimoireBlocks.init();
 		LOGGER.info("ItsLanker is a goon!");
 		LOGGER.info("[CGrimoire] loaded successfully!");
+		Registry.register(BuiltInRegistries.PARTICLE_TYPE, Identifier.fromNamespaceAndPath(MOD_ID, "sparkle_particle"), SPARKLE_PARTICLE);
 	}
 
 	public static Identifier id(String path) {
